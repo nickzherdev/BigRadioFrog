90ffcf5aede71b6f3db133e536e6f21325a67c5d
nickzherdev <nikolay.zherdev@s-telematics.ru>
2022-05-09 23:38:48 +0300

    ENH: clean code, add CRC

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,57 +1,65 @@
 #include <SPI.h>                                          // Подключаем библиотеку  для работы с шиной SPI
 #include <nRF24L01.h>                                     // Подключаем файл настроек из библиотеки RF24
 #include <RF24.h>                                         // Подключаем библиотеку  для работы с nRF24L01+
+#include "RadioHelper.h"
 
 #define PIN_LED 5
 #define LED_POWER 8
+#define SPEED_2 6  // Motor2
+#define DIR_2 7  // Motor2
 
-#define SPEED_2      6  // Motor2
-#define DIR_2        7  // Motor2
-
-RF24 radio(9, 10);                              // Создаём объект radio   для работы с библиотекой RF24, указывая номера выводов nRF24L01+ (CE, CSN)
+RF24 radio(9, 10);  // Создаём объект radio   для работы с библиотекой RF24, указывая номера выводов nRF24L01+ (CE, CSN)
 const uint32_t pipe = 111156789; // адрес рабочей трубы;
-// byte data[1];                                   // Создаём массив для приёма данных
-int recieved_data[1];   // массив принятых данных
+
+struct DataPack {
+  int motorSpeed;
+  byte LED;
+};
+
+DataPack Signal;  // control values
 
 void setup(){
   Serial.begin(9600);
 
-  pinMode(6, OUTPUT);  // Motor2
-  pinMode(7, OUTPUT);  // Motor2
+  // Motor
+  pinMode(SPEED_2, OUTPUT);
+  pinMode(DIR_2, OUTPUT);
 
   pinMode(PIN_LED, OUTPUT);
-  pinMode(LED_POWER, OUTPUT);     // voltage pin for the LED
-  digitalWrite(LED_POWER, HIGH);  // voltage pin for the LED
+  pinMode(LED_POWER, OUTPUT);  // voltage pin for the LED
+  digitalWrite(LED_POWER, HIGH);
 
-  Serial.println("Receiver ON");
-
-  radio.begin();  // инициализация
-  delay(2000);
-  radio.setAutoAck(1);              // режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15);          // (время между попыткой достучаться, число попыток)
-  radio.enableAckPayload();         // разрешить отсылку данных в ответ на входящий сигнал
-  radio.setPayloadSize(32);         // размер пакета, в байтах
-  radio.openReadingPipe(1, pipe);   // открыть трубу на приём
-  radio.setChannel(0x6f);           // установка канала
-  
-  radio.setPALevel (RF24_PA_HIGH);   // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
-  radio.setDataRate(RF24_1MBPS);    // скорость обмена данными RF24_1MBPS или RF24_2MBPS
-//  radio.setCRCLength(RF24_CRC_8); // размер контрольной суммы 8 bit или 16 bit
-  radio.powerUp();                  // начать работу
-  radio.startListening();           // приём
-  Serial.println("Receiver READY");
+  setupRadio();
 }
 
 void loop(){
+
   if (radio.available()) {                                
-      radio.read(&recieved_data, sizeof(recieved_data));
-      analogWrite(PIN_LED, abs(recieved_data[0]));
-      // Serial.println(recieved_data[0]);
+      radio.read(&Signal, sizeof(Signal));
+      analogWrite(PIN_LED, Signal.LED);
       delay(100);
-      recieved_data[0] < 0 ? digitalWrite(DIR_2, LOW) : digitalWrite(DIR_2, HIGH);
-      analogWrite(SPEED_2, abs(recieved_data[0])/5);
+      Signal.motorSpeed < 0 ? digitalWrite(DIR_2, LOW) : digitalWrite(DIR_2, HIGH);
+      analogWrite(SPEED_2, abs(Signal.motorSpeed)/3);
   } else {
       analogWrite(PIN_LED, 0);
       analogWrite(SPEED_2, 0);
   }
+}
+
+void setupRadio(void) {
+  radio.begin();  // инициализация
+  Serial.println("Receiver ON");
+  delay(2000);
+  radio.setAutoAck(1);              // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15);          // (время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();         // разрешить отсылку данных в ответ на входящий сигнал, не работает на скорости 250 kbps!
+  radio.setPayloadSize(32);         // размер пакета, в байтах
+  radio.openReadingPipe(1, pipe);   // открыть трубу на приём
+  radio.setChannel(0x6f);           // установка канала
+  radio.setPALevel (RF24_PA_MIN);   // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate(RF24_1MBPS);    // скорость обмена данными RF24_1MBPS или RF24_2MBPS или RF24_250KBPS (не поддерживает enableAckPayload)
+  radio.setCRCLength(RF24_CRC_16); // размер контрольной суммы 8 bit или 16 bit,  
+  radio.powerUp();                  // начать работу
+  radio.startListening();           // приём
+  Serial.println("Receiver READY");
 }
--- src/transmitter.cpp
+++ src/transmitter.cpp
@@ -1,157 +1,109 @@
-// While button is Holded - LED is ON
-// When button is released - LED is off
-
-/*   Данный скетч делает следующее: передатчик (TX) отправляет массив
-     данных, который генерируется согласно показаниям с кнопки. 
-     Это может быть нажатие кнопки или удержание.
-     
-     Приёмник (RX) получает массив и генерирует ШИМ сигнал на светодиод.
-     Зажигание один раз или удержание в светящем состоянии.
-    by AlexGyver 2016
-*/
-
 #include <SPI.h>                                          // Подключаем библиотеку для работы с шиной SPI
 #include <RF24.h>                                         // Подключаем библиотеку для работы с nRF24L01+
 #include "GyverButton.h"
 #include "printf.h"
 #include <GyverJoy.h>
+#include "RadioHelper.h"
+#include "JoystickHelper.h"
 
 #define LED_PIN 5
-#define BTN_PIN 4  // кнопка подключена сюда (BTN_PIN --- КНОПКА --- GND)
-
-#define JSW 2
 
 GyverJoy jx(0);   // джойстик на пине A0
-// GyverJoy jy(1);   // джойстик на пине A1
-// GButton jsw(2);   // кнопка джойстика на пине 2
-
-GButton butt1(BTN_PIN);  // Trema-кнопка со светодиодом
+GButton jsw(3);   // кнопка джойстика
 
 RF24 radio(9, 10);  // Создаём объект radio для работы с библиотекой RF24, указывая номера выводов nRF24L01+ порты D9, D10: CSN CE
 const uint32_t pipe = 111156789; // адрес рабочей трубы;
 
-int transmit_data[1];     // массив пересылаемых данных
-// byte data[1];  // Создаём массив для приёма данных
-int flag = 0;
-int value = 0;
+struct DataPack {
+  int motorSpeed;
+  byte LED;
+};
 
-int JSW_state = 0;
+DataPack Signal;  // control values
 int user_input_jx = 0;
+bool flag = 0;
 
 void setup(){
 
   Serial.begin(9600);
-
-                            //////////////////////////////
-                            //////// SET UP BUTTON ///////
-                            //////////////////////////////
-  butt1.setDebounce(50);        // настройка антидребезга (по умолчанию 80 мс)
-  butt1.setTimeout(500);        // настройка таймаута на удержание (по умолчанию 500 мс)
-  butt1.setClickTimeout(200);   // настройка таймаута между кликами (по умолчанию 300 мс)
-
-  // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (BTN_PIN --- КНОПКА --- GND)
-  // LOW_PULL  - кнопка подключена к VCC, пин подтянут к GND
-  // по умолчанию стоит HIGH_PULL
-  butt1.setType(LOW_PULL);
-
-  // NORM_OPEN - нормально-разомкнутая кнопка
-  // NORM_CLOSE - нормально-замкнутая кнопка
-  // по умолчанию стоит NORM_OPEN
-  butt1.setDirection(NORM_OPEN);
-
-  // Joystick button
-  // jsw.setType(LOW_PULL);
-  // jsw.setDebounce(50);        // настройка антидребезга (по умолчанию 80 мс)
-  // jsw.setTimeout(500);        // настройка таймаута на удержание (по умолчанию 500 мс)
-  // jsw.setClickTimeout(200);   // настройка таймаута между кликами (по умолчанию 300 мс)
-  // jsw.setDirection(NORM_OPEN);
-  pinMode(JSW, INPUT_PULLUP);
-
-  jx.calibrate();   // калибровка нуля при запуске
-  jx.deadzone(10);  // мёртвая зона
-  jx.exponent(GJ_CUBIC);  // экспонента для плавности
- 
-  // jy.calibrate();   // калибровка нуля при запуске
-  // jy.deadzone(10);  // мёртвая зона
-  // jy.exponent(GJ_CUBIC);  // экспонента для плавности
-
-  //////////////////////////////
-  ///////// SET UP LED /////////
-  //////////////////////////////
   pinMode(LED_PIN, OUTPUT);
 
-  //////////////////////////////
-  //////// SET UP RADIO ////////
-  //////////////////////////////
-  Serial.println("Transmitter ON");
-  radio.begin();                // инициализация
-  delay(2000);
-  radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15);    // (время между попыткой достучаться, число попыток)
-  radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
-  radio.setPayloadSize(32);   // размер пакета, в байтах
-  radio.openWritingPipe(pipe);   // открыть трубу на отправку
-
-  radio.setDataRate(RF24_1MBPS); // скорость обмена данными RF24_1MBPS или RF24_2MBPS
-//  radio.setCRCLength(RF24_CRC_8); // размер контрольной суммы 8 bit или 16 bit
-  radio.setPALevel(RF24_PA_HIGH); // уровень питания усилителя RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
-  radio.setChannel(0x6f);         // установка канала
-  radio.powerUp();               // включение или пониженное потребление powerDown - powerUp
-  radio.stopListening();  //радиоэфир не слушаем, только передача
-  Serial.println("TransmitterTester READY");
-
+  setupJoystick();
+  setupRadio();
 }
+
 void loop() {
 
-  // тикер опрашивает АЦП по своему таймеру
-  if (jx.tick()) {
-    transmit_data[0] = jx.value();
-    // выводим значение
-    Serial.println(jx.value());
+  if (jx.tick()) {                  // тикер опрашивает АЦП по своему таймеру
+    Signal.motorSpeed = jx.value();
   }
 
-  // if (jsw.isClick()) {
-  //   Serial.println("Joystick Click");         // проверка на один клик
-  // }
+  if (abs(Signal.motorSpeed) > 20 ) {  // Deadzone
+    Signal.LED = (byte)abs(Signal.motorSpeed);
+    radio.powerUp();
+    radio.write(&Signal, sizeof(Signal));
+    radio.powerDown();
+    Serial.println("Sent");
+  } else {
+      analogWrite(LED_PIN, 0);
+  }
 
-  JSW_state = digitalRead(JSW);
-  // Serial.print(" | Button: ");
-  // Serial.println(!JSW_state);
+  jsw.tick();  // обязательная функция отработки. Должна постоянно опрашиваться
 
-  // data[0] = 50;
-
-  butt1.tick();  // обязательная функция отработки. Должна постоянно опрашиваться
-
-  if (butt1.isClick()) {
-    Serial.println("Click");         // проверка на один клик
+  if (jsw.isClick()) {
+    Serial.println("Joystick Click");
     flag = 1;
   }
 
-  if (flag == 1) {
-    radio.powerUp();    // включить передатчик
-    radio.write(&transmit_data, sizeof(transmit_data));
-    flag = 0;           //опустить флаг
-    radio.powerDown();  // выключить передатчик
-    Serial.println("Sent from click");         // проверка на один клик
+  if (flag) {
+    Signal.LED = 100;
+    radio.powerUp();
+    radio.write(&Signal, sizeof(Signal));
+    radio.powerDown();
+    Serial.println("Sent from click");
+    flag = 0;
   }
 
-  if (butt1.isStep()) {            // обработчик удержания с шагами
-    value++;                       // увеличивать/уменьшать переменную value с шагом и интервалом!
-    Serial.println(value);         // для примера выведем в порт
+  if (jsw.isHold()) {
+    Serial.println("Holding ");
+    Signal.LED = 100;
+    Signal.motorSpeed = 0;
+    radio.powerUp();
+    radio.write(&Signal, sizeof(Signal));
+    radio.powerDown();
+    Serial.println("Sent from click");
   }
-  if (butt1.isRelease()) {
-    value = 0;
-    Serial.println("Release");         // для примера выведем в порт
-  }
+}
 
-  if (butt1.isHold()) {                                  // если кнопка удерживается
-    Serial.println("Holding ");                  // выводим пока удерживается
-    analogWrite(LED_PIN, 20);
-    radio.powerUp();    // включить передатчик
-    radio.write(&transmit_data, sizeof(transmit_data));
-    radio.powerDown();  // выключить передатчик
-    Serial.println("Sent");         // проверка на один клик
-  } else {
-    analogWrite(LED_PIN, 0);
-  }
+void setupJoystick(void) {
+  // HIGH_PULL - (по умолчанию) кнопка подключена к GND, пин подтянут к VCC (BTN_PIN --- КНОПКА --- GND)
+  // LOW_PULL  - кнопка подключена к VCC, пин подтянут к GND
+  // NORM_OPEN - (по умолчанию) нормально-разомкнутая кнопка
+  // NORM_CLOSE - нормально-замкнутая кнопка
+  jsw.setType(HIGH_PULL);
+  jsw.setDebounce(50);        // настройка антидребезга (по умолчанию 80 мс)
+  jsw.setTimeout(500);        // настройка таймаута на удержание (по умолчанию 500 мс)
+  jsw.setClickTimeout(300);   // настройка таймаута между кликами (по умолчанию 300 мс)
+
+  jx.calibrate();   // калибровка нуля при запуске
+  jx.deadzone(20);  // мёртвая зона
+  jx.exponent(GJ_CUBIC);  // экспонента для плавности
+}
+
+void setupRadio(void) {
+  radio.begin();                    // инициализация
+  Serial.println("Transmitter ON");
+  delay(2000);
+  radio.setAutoAck(1);              // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15);          // (время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();         // разрешить отсылку данных в ответ на входящий сигнал
+  radio.setPayloadSize(32);         // размер пакета, в байтах
+  radio.openWritingPipe(pipe);      // открыть трубу на отправку
+  radio.setDataRate(RF24_1MBPS);    // скорость обмена данными RF24_1MBPS или RF24_2MBPS
+  radio.setCRCLength(RF24_CRC_16);  // размер контрольной суммы 8 bit или 16 bit
+  radio.setPALevel(RF24_PA_MIN);    // уровень питания усилителя RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
+  radio.setChannel(0x6f);           // установка канала
+  radio.powerUp();                  // включение или пониженное потребление powerDown - powerUp
+  radio.stopListening();            // радиоэфир не слушаем, только передача
+  Serial.println("TransmitterTester READY");
 }
